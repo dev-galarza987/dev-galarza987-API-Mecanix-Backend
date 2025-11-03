e5309fc09cf65cf80416a1ce2240b7dc8721cf80
Victor Hugo Galarza Jimenez <dev.galarza@gmail.com>
2025-11-03 04:07:12 -0400

    feat(client): add Reservate and ClientVehicle to client module imports.

--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -3,9 +3,11 @@ import { ClientService } from './client.service';
 import { ClientController } from './client.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Client } from './entities/client.entity';
+import { Reservate } from '../reservate/entities/reservate.entity';
+import { ClientVehicle } from '../client-vehicle/entities/client-vehicle.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Client])],
+  imports: [TypeOrmModule.forFeature([Client, Reservate, ClientVehicle])],
   controllers: [ClientController],
   providers: [ClientService],
 })
